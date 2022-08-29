b94e681d221babf95d7663ed852bc6dd6d74c944
emilyaikens <emily.c.aikens@gmail.com>
2022-08-28 20:54:20 -0700

    add google login route, google callback route, and google logout route to routes/index.js

--- mongoose-project-2/routes/index.js
+++ mongoose-project-2/routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+//eventually change to :
+// router.get('/', function(req, res, next) {
+//   res.redirect('/movies');
+// });
+
+//google login route
+router.get('/auth/google', passport.authenticate( //path
+  'google', //strategy - required by .authenticate
+  {scope: ['profile', 'email']} //options - required by .authenticate
+));
+
+//google callback route
+router.get('/oauth2callback', passport.authenticate( //path
+  'google', //strategy
+    {
+      successRedirect: '/movies', //where to send user if the login is successful
+      failureRedirect: '/movies' //where to send user if login fails
+    }
+));
+
+//google logout
+router.get("/logout", (req, res) => {
+  req.logout(req.user, err => {
+      if (err) return next(err);
+      res.redirect("/");
+  });
 });
 
 module.exports = router;
