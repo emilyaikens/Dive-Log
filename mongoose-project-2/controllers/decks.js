9c39db701073db78912c823b140cef106a585112
emilyaikens <emily.c.aikens@gmail.com>
2022-08-29 17:10:39 -0700

    add create function to controllers/decks

--- mongoose-project-2/controllers/decks.js
+++ mongoose-project-2/controllers/decks.js
@@ -4,11 +4,19 @@ function index(req, res) {
     Deck.find({}, function(err, decks) {
       res.render('decks/index', { title: 'All Decks', decks });
     });
-  }
+};
 
 function newDeck (req, res) {
     res.render('decks/new', {title: "New Deck"});
-}
+};
+
+function create (req,res) {
+    const deck = new Deck(req.body);
+    deck.save(function(err) {
+        if (err) return res.redirect('/deck/new');
+        res.redirect(`/decks/${deck._id}/edit`);
+    })
+};
 
   module.exports = {
     index,
