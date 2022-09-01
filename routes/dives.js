b358ce6c4c437727fce0a30177df2e85eb5d158a
emilyaikens <emily.c.aikens@gmail.com>
2022-09-01 00:12:13 -0700

    add more comments

--- routes/dives.js
+++ routes/dives.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const divesCtrl = require('../controllers/dives');
 const isLoggedIn = require('../config/auth');
 
-router.get('/', isLoggedIn, divesCtrl.index);
-router.get('/new', isLoggedIn, divesCtrl.new);
-router.post('/', isLoggedIn, divesCtrl.create);
-router.get('/:id', isLoggedIn, divesCtrl.show);
-router.get('/:id/edit', isLoggedIn, divesCtrl.edit);
-router.put('/:id/update', isLoggedIn, divesCtrl.update);
+router.get('/', isLoggedIn, divesCtrl.index); 
+router.get('/new', isLoggedIn, divesCtrl.new); 
+router.post('/', isLoggedIn, divesCtrl.create); 
+router.get('/:id', isLoggedIn, divesCtrl.show); 
+router.get('/:id/edit', isLoggedIn, divesCtrl.edit); 
+router.put('/:id/update', isLoggedIn, divesCtrl.update); 
 router.delete('/:id/delete', isLoggedIn, divesCtrl.delete);
 
 module.exports = router;
--- routes/profiles.js
+++ routes/profiles.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const profilesCtrl = require('../controllers/profiles');
 const isLoggedIn = require('../config/auth');
 
-router.get('/', isLoggedIn, profilesCtrl.index);
-router.get('/new', isLoggedIn, profilesCtrl.new);
-router.post('/', isLoggedIn, profilesCtrl.create);
-router.get('/edit', isLoggedIn, profilesCtrl.edit);
-router.put('/:id/update', isLoggedIn, profilesCtrl.update);
+router.get('/', isLoggedIn, profilesCtrl.index); //called from the "my profile" link in header
+router.get('/new', isLoggedIn, profilesCtrl.new); //called from Add Information link in the index view
+router.post('/', isLoggedIn, profilesCtrl.create); //called from the form in the new view
+router.get('/edit', isLoggedIn, profilesCtrl.edit); //called from the index view, link to edit profile view
+router.put('/:id/update', isLoggedIn, profilesCtrl.update); //called from form in edit profile view
 
 module.exports = router;
